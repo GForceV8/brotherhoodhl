5f09314dd9720b8b9bf57bb326043cadd1364d4f
GForceV8 <37455675+GForceV8@users.noreply.github.com>
2020-04-22 06:29:31 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -197,7 +197,7 @@ bot.on("message", async function(message) {
 		        	    
             case "unit":
             var embed = new Discord.RichEmbed()
-                .addField("Groupe Unit ** @Full Patch **", "Voici votre unit @699005379102638182 https://units.arma3.com/unit/brotherhood-hl")
+                .addField("Groupe Unit ** @Full Patch **", "Voici votre unit @Full Patch https://units.arma3.com/unit/brotherhood-hl")
                 .setColor("#3b9908")
                 .setFooter("Unit - Brotherhood by @GForceV8")
                 .setAuthor(message.author.username, message.author.avatarURL)
